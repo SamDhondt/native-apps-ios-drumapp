dd4560924e2402e0ae87c1c694962db2666507f8
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2018-11-16 16:47:30 +0100

    Add method to find favorite rudiment in Progress

--- Click/Progress/Progress.swift
+++ Click/Progress/Progress.swift
@@ -11,4 +11,10 @@ import RealmSwift
 
 class Progress: Object {
     var practiceSessions: List<PracticeSession> = List()
+    
+    func getFavoriteRudimentName() -> String {
+        let rudiments = practiceSessions.map({ $0.rudiment!.name })
+        let rudimentCount = rudiments.reduce(into: [:]) { $0[$1, default: 0] += 1 }
+        return rudimentCount.max(by: { $0.1 < $1.1 } )?.key ?? "No favorite rudiment, go practice some more!"
+    }
 }
