2a3c710450da35897d63fb79bafe9f45ac8db5c4
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2018-11-17 16:28:25 +0100

    Add ViewController for Progress

--- Click/Progress/ProgressViewController.swift
+++ Click/Progress/ProgressViewController.swift
@@ -9,23 +9,13 @@
 import UIKit
 
 class ProgressViewController: UIViewController {
-    let dataManager = DataManager()
+    private let dataManager = DataManager()
+    private var progress = Progress()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        progress = dataManager.getProgress()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
