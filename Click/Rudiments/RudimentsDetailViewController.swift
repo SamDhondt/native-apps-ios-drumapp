b2319eb5b5d943fcfcd5f212fe96bf366f3a7920
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2019-01-20 14:15:51 +0100

    Hide empty row lines in table views

--- Click/Rudiments/RudimentsDetailViewController.swift
+++ Click/Rudiments/RudimentsDetailViewController.swift
@@ -14,6 +14,7 @@ class RudimentsDetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var stickingLabel: UILabel!
     @IBOutlet weak var commentsTableView: UITableView!
+    @IBOutlet weak var progress: UIActivityIndicatorView!
     
     var rudiment: Rudiment?
     var comments = [Comment]()
@@ -23,20 +24,26 @@ class RudimentsDetailViewController: UIViewController {
         
         commentsTableView.dataSource = self
         commentsTableView.delegate = self
+        // hides excess empty rows
+        commentsTableView.tableFooterView = UIView(frame: CGRect.zero)
+        commentsTableView.backgroundColor = UIColor.clear
         
         updateUI()
     }
     
     private func updateUI() {
-        loadViewIfNeeded()
         nameLabel.text = rudiment?.name
         stickingLabel.text = rudiment?.sticking
         retrieveCommentsFromApi()
     }
     
     private func retrieveCommentsFromApi() {
+        progress.startAnimating()
+        commentsTableView.isHidden = true
         if let rud = rudiment {
             _ = DrumAPI.retrieveComments(forRudiment: rud).subscribe(onNext: { comments in
+                self.progress.stopAnimating()
+                self.commentsTableView.isHidden = false
                 self.comments.removeAll()
                 self.comments.append(contentsOf: comments)
                 self.commentsTableView.reloadData()
